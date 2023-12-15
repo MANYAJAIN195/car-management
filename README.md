22335a75fc036ccd675bf792912e05dd25fe1fd0
MANYAJAIN195 <manyajain195@gmail.com>
2023-12-16 00:40:06 +0530

    developed CRUD operations

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-GO CAR GARAGE MANAGEMENT
+# GO CAR GARAGE MANAGEMENT
+
+I have developed a mini-project using the GoFr framework to build a simple HTTP API for a Car Garage Management service. The API includes CRUD operations for managing entries of cars in the garage and integrates with a MySQL database using docker for persistent data storage. The key functionalities implemented include adding a new entry to the database when a car enters the garage (capturing owner name, car number, and status), retrieving the list of currently parked cars, fetching details of a specific car based on its number, updating the entry when a car undergoes repair with relevant status parameters, and finally, deleting the entry from the database when the car leaves the garage using its number. Furthermore, activity diagrams and UML diagrams are also created to provide a visual representation of the system's interactions and structure, understanding of the project architecture.
+
+### UML CLASS DIAGRAM
+![Alt text](uml.png)
+
+### UML ACTIVITY DIAGRAM
+![Alt text](<activity diagram.png>)
+
+### Commands to run the program
+
+To download gofr, use the command: 
+```
+go get gofr.dev
+```
+
+To download and sync the required modules, use the command:
+```
+go mod tidy
+```
+
+run the mysql server and create a database locally using the following docker command:
+```
+docker run --name gofr-mysql -e MYSQL_ROOT_PASSWORD=root123 -e MYSQL_DATABASE=test_db -p 3306:3306 -d mysql:8.0.30
+```
+
+Access test_db database and create table car:
+```
+docker exec -it gofr-mysql mysql -uroot -proot123 test_db -e "CREATE TABLE car (id INT AUTO_INCREMENT PRIMARY KEY, Owner VARCHAR(255) NOT NULL, CarNo VARCHAR(255) NOT NULL, Status VARCHAR(255) NOT NULL);"
+```
+
+To run the server, use the command:
+```
+go run main.go
+```
+
+Note: Access the server at port 8080 which can be modified in .env file.
--- main.go
+++ main.go
@@ -1,18 +1,86 @@
 package main
 
-import "gofr.dev/pkg/gofr"
+import (
+	"gofr.dev/pkg/gofr"
+)
+
+type Car struct {
+	ID     int    `json:"ID"`
+	Owner  string `json:"Owner"`
+	CarNo  string `json:"CarNo"`
+	Status string `json:"Status"`
+}
 
 func main() {
-	// initialise gofr object
 	app := gofr.New()
 
-	// register route greet
-	app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
+	app.POST("/caradd/{name}/{number}/{status}", func(ctx *gofr.Context) (interface{}, error) {
+		name := ctx.PathParam("name")
+		number := ctx.PathParam("number")
+		status := ctx.PathParam("status")
 
-		return "Hello World!", nil
+		// Inserting a car row in the database using SQL
+		_, err := ctx.DB().ExecContext(ctx, "INSERT INTO car (Owner, CarNo, Status) VALUES (?, ?, ?)", name, number, status)
+
+		return nil, err
 	})
 
-	// Starts the server, it will listen on the default port 8000.
-	// it can be over-ridden through configs
+	app.GET("/carlist", func(ctx *gofr.Context) (interface{}, error) {
+		var cars []Car
+
+		// Getting cars from the database using SQL
+		rows, err := ctx.DB().QueryContext(ctx, "SELECT * FROM car")
+		if err != nil {
+			return nil, err
+		}
+
+		defer rows.Close()
+
+		for rows.Next() {
+			var car Car
+			if err := rows.Scan(&car.ID, &car.Owner, &car.CarNo, &car.Status); err != nil {
+				return nil, err
+			}
+
+			cars = append(cars, car)
+		}
+
+		// return the cars
+		return cars, nil
+	})
+
+	app.PUT("/carupdate/{number}/{status}", func(ctx *gofr.Context) (interface{}, error) {
+		number := ctx.PathParam("number")
+		status := ctx.PathParam("status")
+
+		// Updating the status of the car in the database using SQL
+		_, err := ctx.DB().ExecContext(ctx, "UPDATE car SET Status = ? WHERE CarNo = ?", status, number)
+
+		return nil, err
+	})
+
+	app.GET("/carinfo/{number}", func(ctx *gofr.Context) (interface{}, error) {
+		number := ctx.PathParam("number")
+
+		// Retrieving car information based on car number from the database using SQL
+		var car Car
+		err := ctx.DB().QueryRowContext(ctx, "SELECT * FROM car WHERE CarNo = ?", number).Scan(&car.ID, &car.Owner, &car.CarNo, &car.Status)
+
+		if err != nil {
+			return nil, err
+		}
+
+		return car, nil
+	})
+
+	app.DELETE("/cardelete/{number}", func(ctx *gofr.Context) (interface{}, error) {
+		number := ctx.PathParam("number")
+
+		// Deleting car information from the database based on car number using SQL
+		_, err := ctx.DB().ExecContext(ctx, "DELETE FROM car WHERE CarNo = ?", number)
+
+		return nil, err
+	})
+
 	app.Start()
 }
